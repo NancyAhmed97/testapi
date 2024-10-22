35d67b76b6569bf6148684812f8bfc15c2d68438
NancyAhmed97 <anancyahmed97@icloud.com>
2024-10-22 04:02:34 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<input
+
+placeholder='email'
+value={email}
+onChange={(e)=>setEmail(e.target.value)}
+/>
+<input
+value={password}
+onChange={(e)=>setPassword(e.target.value)}
+
+
+/>
+<button
+onClick={()=>{
+  axios.post('https://92.113.26.138:8081/api/signin', {email:email,password:password})
+  .then((res) => {
+console.log(res.data);
+
+
+  })
+  .catch((err) => {
+   
+ 
+  });  
+}}
+>ok</button>
     </div>
   );
 }
